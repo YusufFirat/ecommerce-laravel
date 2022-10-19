82cca6960bf2887abb08e5fb2b91b77c71c55127
YusufFirat <yusuffirat321@gmail.com>
2022-10-19 09:27:18 +0300

    User Create page created User Edit page created Some frontend issues fixed

---
+++ app/Http/Controllers/Backend/UserController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        $users = User::all();
+       return view("backend.users.index", ['users' => $users]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       return view("backend.users.insert_form");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $name = $request-> get("name");
+        $email = $request->get("email");
+        $password = $request->get("password");
+        $is_active = $request->get("is_active", 0);
+        $is_admin = $request->get("is_admin",0 );
+
+
+ // Aldığımız Bilgiler İle Yeni User Oluşturuldu
+        $user = new User();
+        $user->name = $name;
+        $user->email = $email;
+        $user->password = $password;
+        $user->is_admin = $is_admin;
+        $user->is_active= $is_active;
+ // Oluşturduğumuz User Kaydedildi
+        $user->save();
+      return redirect()->route('/users');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      return "show";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+        $user = User::find($id);
+      return view("backend.users.update_form",["user"=>$user]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+           $name = $request-> get("name");
+           $email = $request->get("email");
+           $is_active = $request->get("is_active", 0);
+           $is_admin = $request->get("is_admin",0 );
+
+           $user = User::find($id);
+           $user->name = $name;
+           $user->email = $email;
+           $user->is_admin = $is_admin;
+           $user->is_active= $is_active;
+           $user->save();
+          return redirect()->route('/users');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return "destroy";
+    }
+}
---
+++ resources/views/backend/users/insert_form.blade.php
@@ -0,0 +1,186 @@
+
+<!doctype html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Dashboard Users Page </title>
+
+<link rel= "stylesheet" href="{{asset('build/assets/app.91759510.css')}}">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('build/assets/dashboard.css')}}" rel="stylesheet">
+  </head>
+  <body>
+
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Laravel Ecommerce</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
+  <div class="navbar-nav">
+    <div class="nav-item text-nowrap">
+      <a class="nav-link px-3" href="#">Sign out</a>
+    </div>
+  </div>
+</header>
+
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3 sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              <span data-feather="home" class="align-text-bottom"></span>
+              Yönetim Paneli
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/users">
+              <span data-feather="file" class="align-text-bottom"></span>
+              Kullanıcılar
+            </a>
+          </li>
+          <li class="nav-item">
+
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text" class="align-text-bottom"></span>
+
+
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Yönetim Paneli</h1>
+      </div>
+
+
+
+      <h2>Kullanıcılar</h2>
+      <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-group-me-2">
+            <a href="/users/create" class="btn btn-sm btn-outline-danger">Yeni Ekle</a>
+            </div>
+      </div>
+      <h2> Yeni Kullanıcı Ekle </h2>
+      <div class="table-responsive">
+            <form action="{{url("/users")}}" method="POST">
+            <!--Diğer sitelerden Request atılmasını engellemek için kullanılan ve Unique Token oluşturan Güvenlik Amaçlı  -->
+                @csrf
+                <div class="row">
+                <div class="col-lg-6">
+                  <label for="name" class="form-label">Ad Soyad</label>
+                  <input type="name" class="form-control" id="name" name="name" placeholder="Ad Soyad Giriniz" >
+                </div>
+                   <div class="col-lg-6">
+                      <label for="email" class="form-label">E-posta Giriniz</label>
+                      <input type="email" class="form-control" id="email" name="email" placeholder="E-posta Giriniz" >
+                   </div>
+                    <div class="col-lg-6">
+                      <label for="password" class="form-label">Şifre Giriniz</label>
+                      <input type="password" class="form-control" id="password" name="password" placeholder="Şifre Giriniz">
+                   </div>
+                    <div class="col-lg-6" >
+                      <label for="password2" class="form-label">Şifreyi Tekrar Giriniz</label>
+                      <input type="password" class="form-control" id="password2" name="password2" placeholder="Şifreyi Tekrar Giriniz">
+                    </div>
+                    <div class="row">
+                    <div class="col-lg-6 mt-2 ">
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox"  id="is_admin" name="is_admin" value="1">
+                          <label class="is_admin" for="is_admin">
+                           Yetkili Kullanıcı
+                          </label>
+                        </div>
+                        </div>
+                        <div class="col-lg-6 mt-2"  >
+                            <div class="form-check">
+                          <input class="form-check-input" type="checkbox" id="is_active" name="is_active" value="1">
+                          <label class="is_active" for="is_active">
+                           Aktif Kullanıcı
+                          </label>
+                        </div>
+                    </div>
+                </div>
+                <div class=row>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-success mt-3">Kaydet</button>
+                    </div>
+                </div>
+            </form>
+
+      </div>
+    </main>
+  </div>
+</div>
+
+
+<script type="text/javascript" src="{{asset('build/assets/app.8cd8d675.js')}}"></script>
+  </body>
+</html>
+
